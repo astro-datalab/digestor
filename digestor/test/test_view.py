9795f7789e07ead6b6161f194aefe1237817e71e
Benjamin Alan Weaver <baweaver@lbl.gov>
2020-01-28 19:24:51 -0700

    fix tests of view.py

--- digestor/test/test_view.py
+++ digestor/test/test_view.py
@@ -5,6 +5,9 @@
 import unittest
 import unittest.mock as mock
 
+from .utils import DigestorCase
+from ..view import get_options
+
 
 class TestView(DigestorCase):
     """Test digestor.view.
@@ -12,6 +15,19 @@ class TestView(DigestorCase):
 
     def setUp(self):
         super().setUp()
+
+    def test_get_options(self):
+        """Test command-line arguments.
+        """
+        with mock.patch('sys.argv', ['add_view_metdata', '-d', 'Test', '-s', 'sdss',
+                                     'sdss_dr14.json', 'specobjall', 'specobj']):
+            self.options = get_options()
+        self.assertEqual(self.options.meta, 'sdss_dr14.json')
+        self.assertEqual(self.options.table, 'specobjall')
+        self.assertEqual(self.options.view, 'specobj')
+        self.assertEqual(self.options.schema, 'sdss')
+        self.assertEqual(self.options.description, 'Test')
+        self.assertIsNone(self.options.output)
 
 
 def test_suite():
